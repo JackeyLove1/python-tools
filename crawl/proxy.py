07275d829a5e5bcc3d383f69b0fa5e884a2f58ea
fanhuanjie <fanhuanjie@bytedance.com>
2023-07-29 23:48:52 +0800

    fix

--- crawl/proxy.py
+++ crawl/proxy.py
@@ -1,9 +1,19 @@
 import requests
-# define proxies
+# use proxies
 proxies = {
-        'http' : 'http://xx.xxx.xxx.xxx:xxxx',
-        'http' : 'http://xxx.xx.xx.xxx:xxx',
-        ....
-    }
-# use proxy
-response = requests.get(url,proxies=proxies)
+    "http": "http://127.0.0.1:8787",
+    "https": "http://127.0.0.1:8787",
+}
+url = 'http://httpbin.org/ip'
+r = requests.get(url, proxies=proxies)
+print(r.text)
+# requests from version 2.10.0 support socks proxy
+# pip install -U requests[socks]
+proxies = {'http': "socks5://myproxy:9191"}
+requests.get('http://example.org', proxies=proxies)
+# tornado proxy demo
+# sudo apt-get install libcurl-dev librtmp-dev
+import requests
+
+ip = requests.get('https://api.ipify.org').text
+print(f'My IP address is: {ip}')
---
+++ crawl/thread_crawer.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+import bs4
+import requests
+class ThreadPoolCrawler(object):
+    def __init__(self, urls, concurrency=10, **kwargs):
+        self.urls = urls
+        self.concurrency = concurrency
+        self.results = []
+    def handle_response(self, url, response):
+        pass
+    def get(self, *args, **kwargs):
+        return requests.get(*args, **kwargs)
+    def run(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.concurrency) as executor:
+            future_to_url = {
+                executor.submit(self.get, url): url for url in self.urls
+            }
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    response = future.result()
+                except Exception as e:
+                    import traceback
+                    traceback.print_exc()
+                else:
+                    self.handle_response(url, response)
+class TestCrawler(ThreadPoolCrawler):
+    def handle_response(self, url, response):
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        title = soup.find('title')
+        self.results.append({url: title})
+def main():
+    import time
+    urls = ['http://localhost:8000'] * 300
+    for nums in [2, 5, 10, 15, 20, 50, 70, 100]:
+        beg = time.time()
+        s = TestCrawler(urls, nums)
+        s.run()
+        print(nums, time.time()-beg)
+if __name__ == '__main__':
+    main()
