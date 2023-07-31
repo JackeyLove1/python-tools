1724f91893c2e20ba441ed3ee0f93a3a19031c17
fanhuanjie <fanhuanjie@bytedance.com>
2023-07-31 21:03:25 +0800

    add bytes_io

--- common/data_process/byteio.py
+++ common/data_process/byteio.py
@@ -1,7 +1,7 @@
-from io import BytesIO
-
-file_path = "test.txt"
-
+# Write the modified data back to the file
+'''
+with open(file_path, 'wb') as f:
+    f.write(modified_data)
 with open(file_path, 'rb') as f:
     # Read the data from the file
     data = f.read()
@@ -9,9 +9,87 @@ with open(file_path, 'rb') as f:
     byte_stream = BytesIO(data)
     modified_data = byte_stream.getvalue()
     print(modified_data)
+提取某一个文件所有的文本，然后交给langchain切割文本的函数进行流水线处理
+'''
+import io
+from io import BytesIO
+import os
 
-# Write the modified data back to the file
-'''
-with open(file_path, 'wb') as f:
-    f.write(modified_data)
-'''
+import docx
+import langchain
+import openai
+from typing import List, Any, Optional
+import re
+import docx2txt
+from langchain.docstore.document import Document
+import fitz
+from hashlib import md5
+from abc import abstractmethod, ABC
+from copy import deepcopy
+import docx2txt
+
+
+class File(ABC):
+    def __init__(self,
+                 id: str,
+                 name: Optional[str] = None,
+                 meta: Optional[dict[str, Any]] = None,
+                 docs: Optional[str] = None):
+        self.id = id
+        self.name = name or ""
+        self.meta = meta or {}
+        self.docs = docs or ""  # all raw text
+
+    @classmethod
+    @abstractmethod
+    def from_bytes(cls, files: BytesIO) -> "File":
+        pass
+
+    @staticmethod
+    def read_file_to_byte(file_path: str) -> (BytesIO, bool):
+        byte_io = None
+        try:
+            with open(file_path, 'rb') as file:
+                byte_io = io.BytesIO(file.read())
+            byte_io.seek(0)
+            return byte_io, True
+        except Exception as e:
+            return byte_io, False
+
+    def __repr__(self):
+        return f"name:{self.name}, id:{self.id}, docs:{str(self.docs)}"
+
+
+def strip_consecutive_newlines(text: str) -> str:
+    """Strips consecutive newlines from a string
+    possibly with whitespace in between
+    """
+    return re.sub(r"\s*\n\s*", "\n", text)
+
+
+# package list: docx(complex), docx2txt(simple)
+class DocxFile(File):
+    @classmethod
+    def from_bytes(cls, files: BytesIO) -> "DocxFile":
+        text = docx2txt.process(files)
+        docs = strip_consecutive_newlines(text)
+        return cls(id=md5(files.read()).hexdigest(), docs=docs)
+
+
+class TxtFile(File):
+    @classmethod
+    def from_bytes(cls, files: BytesIO) -> "TxtFile":
+        files.seek(0)
+        text = files.read().decode("utf-8")
+        text = strip_consecutive_newlines(text)
+        docs = text.strip()  # TODO: consider "\n"
+        return cls(id=md5(files.read()).hexdigest(), docs=docs)
+
+
+docx_file_path = "test.docx"
+docx_file, _ = File.read_file_to_byte(docx_file_path)
+print(DocxFile.from_bytes(docx_file))
+
+txt_file_path = "test.txt"
+file, _ = File.read_file_to_byte(txt_file_path)
+print(TxtFile.from_bytes(file))
--- crawl/case/baidutieba.py
+++ crawl/case/baidutieba.py
@@ -11,18 +11,18 @@ def GetTimeByArticle(url):
     resHtml = response.read()
     html = etree.HTML(resHtml)
     time = html.xpath('//span[@class="tail-info"]')[1].text
-    print time
+    print(time)
     return time
 def main():
     output = codecs.open('tieba0812.json', 'w', encoding='utf-8')
     for pn in range(0, 250, 50):
         kw = u'网络爬虫'.encode('utf-8')
         url = 'http://tieba.baidu.com/f?kw=' + urllib.quote(kw) + '&ie=utf-8&pn=' + str(pn)
-        print url
+        print(url)
         request = urllib3.Request(url)
         response = urllib3.urlopen(request)
         resHtml = response.read()
-        print resHtml
+        print(resHtml)
         html_dom = etree.HTML(resHtml)
         # print etree.tostring(html_dom)
         html = html_dom
@@ -35,18 +35,18 @@ def main():
             jieshao = site.xpath('.//*[@class="threadlist_abs threadlist_abs_onlyline "]')[0].text.strip()
             author = site.xpath('.//*[@class="frs-author-name j_user_card "]')[0].text.strip()
             lastName = site.xpath('.//*[@class="frs-author-name j_user_card "]')[1].text.strip()
-            print title, jieshao, Article_url, author, lastName
+            print(title, jieshao, Article_url, author, lastName)
             item = {}
             item['title'] = title
             item['author'] = author
             item['lastName'] = lastName
             item['reply_date'] = reply_date
-            print item
+            print(item)
             line = json.dumps(item, ensure_ascii=False)
-            print line
-            print type(line)
+            print(line)
+            print(type(line))
             output.write(line + "\n")
         output.close()
-    print 'end'
+    print('end')
 if __name__ == '__main__':
     main()
