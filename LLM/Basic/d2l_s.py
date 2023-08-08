76fa6a541de7043f4cc69792a80d390e4924312d
fanhuanjie <fanhuanjie@bytedance.com>
2023-08-08 19:51:41 +0800

    NLP study

--- LLM/Basic/d2l_s.py
+++ LLM/Basic/d2l_s.py
@@ -14,3 +14,30 @@ class CenteredLayer(nn.Module):
 layer = CenteredLayer()
 print(layer(torch.tensor([1, 2, 3, 4, 5], dtype=torch.float32)))
 
+class Car:
+    def __init__(self, color, brand):
+        self.color = color
+        self.brand = brand
+car = Car("blue", "Toyota")
+car_color = getattr(car, "color")
+print(car_color)
+car_brand = getattr(car, "brand")
+print(car_brand)
+
+from d2l import torch as d2l
+import collections
+import re
+d2l.DATA_HUB['time_machine'] = (d2l.DATA_URL + 'timemachine.txt',
+                                '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+def read_time_machine():  #@save
+    """将时间机器数据集加载到文本行的列表中"""
+    with open("../data/timemachine.txt", 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+lines = read_time_machine()
+print(f'# 文本总行数: {len(lines)}')
+print(lines[0])
+print(lines[10])
+
