ed6b31afddf5b3e0ecdf3ef231b27cd9a7e0b379
fanhuanjie <fanhuanjie@bytedance.com>
2023-11-08 00:03:58 +0800

    all

---
+++ storage/files.py
@@ -0,0 +1,4 @@
+import os
+print(os.path.getctime("files"))
+print(os.path.getmtime("files"))
+print(os.path.getatime("files"))
---
+++ storage/htmlREbk.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'example.com'
+resp = requests.get(url)
+soup = BeautifulSoup(resp.content, "html.parser")
+links = soup.find_all("a")
+for link in links:
+    href = link.get("href")
+    print(href)
